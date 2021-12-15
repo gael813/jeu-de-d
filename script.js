2a6a8a0b1a37158f6fc53cdb06495b3ed6c9f85a
Unforgivent <gael.leulier@orange.fr>
2021-12-15 21:56:24 +0100

    add sound and dynamic dice

--- script.js
+++ script.js
@@ -9,25 +9,26 @@ let secondDice = document.getElementById("secondDice");
 let thirdDice = document.getElementById("thirdDice");
 let fourthDice = document.getElementById("fourthDice");
 let fifthDice = document.getElementById("fifthDice");
-let sicthDice = document.getElementById("sicthDice");
+let sixthDice = document.getElementById("sixthDice");
 
 // Random number 
 function getRandomInt(max) 
 {
-    return Math.floor(Math.random() * max);
+    return Math.floor(Math.random() * max + 1);
 }
 
 // Display players selection
 function getPlayersChoice(getRandomInt)
 {
-    if (getRandomInt == 0)
+    let players = null;
+    if (getRandomInt == 1)
     {
         player1ScoreTotal = document.getElementById("totalScorePlayer1");
         currentScorePlayer1 = document.getElementById("currentScorePlayer1");
         document.getElementById("dotSelectedPlayer2").style.display = "none";
         document.getElementById("divided").style.left = "5%";
         document.getElementById("currentPlayer2Div").style.backgroundColor = "grey";
-        console.log(player1ScoreTotal);
+        player = "player1";
     }
     else
     {
@@ -36,31 +37,47 @@ function getPlayersChoice(getRandomInt)
         document.getElementById("dotSelectedPlayer1").style.display = "none";
         document.getElementById("divided").style.left = "50%";
         document.getElementById("currentPlayer1Div").style.backgroundColor = "grey";
+        player = "player2";
     }
 }
+getPlayersChoice(getRandomInt(2));
 
+// Reset to Game
 buttonNewGame = document.getElementById("buttonNewGame").addEventListener("click", newGame);
-
 function newGame() 
 {
     player1ScoreTotal = document.getElementById("totalScorePlayer1");
     player1ScoreTotal.innerHTML = 0;
 }
 
-buttonRollDice = document.getElementById("buttonRollDice")
-buttonRollDice.addEventListener("click", rollDice);
+// Roll of the dice
+buttonRollDice = document.getElementById("buttonRollDice").addEventListener("click", function()
+{
+    rollDice(getRandomInt(6));
+});
 
 function rollDice(getRandomInt) 
 {
-    getRandomInt + 1;
+    let audio = document.getElementById("audio");
+    audio.play();
     if (getRandomInt == 1)
     {
         firstDice.style.display = "flex";
         secondDice.style.display = "none";
         thirdDice.style.display = "none";
         fourthDice.style.display = "none";
-        firstDice.style.display = "none";
+        fifthDice.style.display = "none";
         sixthDice.style.display = "none";
+        if (player == "player1")
+        {
+            currentScorePlayer1 = document.getElementById("currentScorePlayer1");
+            currentScorePlayer1.innerHTML = 0;
+        }
+        else if (player == "player2")
+        {
+            currentScorePlayer2 = document.getElementById("currentScorePlayer2");
+            currentScorePlayer2.innerHTML = 0;
+        }
     }
     else if (getRandomInt == 2)
     {        
@@ -68,8 +85,20 @@ function rollDice(getRandomInt)
         secondDice.style.display = "flex";
         thirdDice.style.display = "none";
         fourthDice.style.display = "none";
-        firstDice.style.display = "none";
+        fifthDice.style.display = "none";
         sixthDice.style.display = "none";
+        if (player == "player1")
+        {
+            currentScorePlayer1 = document.getElementById("currentScorePlayer1");
+            currentScorePlayer1 = currentScorePlayer1 + 2;
+            currentScorePlayer1.innerHTML = currentScorePlayer1;
+        }
+        else if (player == "player2")
+        {
+            currentScorePlayer2 = document.getElementById("currentScorePlayer2");
+            currentScorePlayer2 = currentScorePlayer2 + 2;
+            currentScorePlayer2.innerHTML = currentScorePlayer2;
+        }
     } 
     else if (getRandomInt == 3)
     {        
@@ -77,8 +106,20 @@ function rollDice(getRandomInt)
         secondDice.style.display = "none";
         thirdDice.style.display = "flex";
         fourthDice.style.display = "none";
-        firstDice.style.display = "none";
+        fifthDice.style.display = "none";
         sixthDice.style.display = "none";
+        if (player == "player1")
+        {
+            currentScorePlayer1 = document.getElementById("currentScorePlayer1");
+            currentScorePlayer1 = currentScorePlayer1 + 3;
+            currentScorePlayer1.innerHTML = currentScorePlayer1;
+        }
+        else if (player == "player2")
+        {
+            currentScorePlayer2 = document.getElementById("currentScorePlayer2");
+            currentScorePlayer2 = currentScorePlayer2 + 3;
+            currentScorePlayer2.innerHTML = currentScorePlayer2;
+        }
     }
     else if (getRandomInt == 4)
     {        
@@ -86,8 +127,20 @@ function rollDice(getRandomInt)
         secondDice.style.display = "none";
         thirdDice.style.display = "none";
         fourthDice.style.display = "flex";
-        firstDice.style.display = "none";
+        fifthDice.style.display = "none";
         sixthDice.style.display = "none";
+        if (player == "player1")
+        {
+            currentScorePlayer1 = document.getElementById("currentScorePlayer1");
+            currentScorePlayer1 = currentScorePlayer1 + 4;
+            currentScorePlayer1.innerHTML = currentScorePlayer1;
+        }
+        else if (player == "player2")
+        {
+            currentScorePlayer2 = document.getElementById("currentScorePlayer2");
+            currentScorePlayer2 = currentScorePlayer2 + 4;
+            currentScorePlayer2.innerHTML = currentScorePlayer2;
+        }
     } 
     else if (getRandomInt == 5)
     {        
@@ -95,8 +148,20 @@ function rollDice(getRandomInt)
         secondDice.style.display = "none";
         thirdDice.style.display = "none";
         fourthDice.style.display = "none";
-        firstDice.style.display = "flex";
+        fifthDice.style.display = "flex";
         sixthDice.style.display = "none";
+        if (player == "player1")
+        {
+            currentScorePlayer1 = document.getElementById("currentScorePlayer1");
+            currentScorePlayer1 = currentScorePlayer1 + 5;
+            currentScorePlayer1.innerHTML = currentScorePlayer1;
+        }
+        else if (player == "player2")
+        {
+            currentScorePlayer2 = document.getElementById("currentScorePlayer2");
+            currentScorePlayer2 = currentScorePlayer2 + 5;
+            currentScorePlayer2.innerHTML = currentScorePlayer2;
+        }
     } 
     else if (getRandomInt == 6)
     {        
@@ -104,8 +169,20 @@ function rollDice(getRandomInt)
         secondDice.style.display = "none";
         thirdDice.style.display = "none";
         fourthDice.style.display = "none";
-        firstDice.style.display = "none";
+        fifthDice.style.display = "none";
         sixthDice.style.display = "flex";
+        if (player == "player1")
+        {
+            currentScorePlayer1 = document.getElementById("currentScorePlayer1");
+            currentScorePlayer1 = currentScorePlayer1 + 6;
+            currentScorePlayer1.innerHTML = currentScorePlayer1;
+        }
+        else if (player == "player2")
+        {
+            currentScorePlayer2 = document.getElementById("currentScorePlayer2");
+            currentScorePlayer2 = currentScorePlayer2 + 6;
+            currentScorePlayer2.innerHTML = currentScorePlayer2;
+        }
     }
 }
 
@@ -119,6 +196,3 @@ function getPlayers(getPlayersChoice)
     
 }
 
-getPlayersChoice(getRandomInt(2));
-
-console.log(rollDice(getRandomInt(5)));
